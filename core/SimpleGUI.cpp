db6adb1695c29be081bd043d74950e5956daa9be
Christophe Guébert <christophe.guebert@gmail.com>
2015-09-16 09:28:11 +0200

    Forget to add a file.

---
+++ core/SimpleGUI.cpp
@@ -0,0 +1,22 @@
+#include <core/SimpleGUI.h>
+
+namespace ui
+{
+
+Panel::~Panel() 
+{
+}
+
+Dialog::~Dialog() 
+{
+}
+
+Settings::~Settings()
+{
+}
+
+SimpleGUI::~SimpleGUI()
+{
+}
+
+} // namespace ui
