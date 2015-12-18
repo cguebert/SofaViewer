ca5e1453f95fcc53fd0f312aca490d9f45731940
Christophe Guébert <christophe.guebert@gmail.com>
2015-12-18 23:12:40 +0100

    Detect types that do not have stream operators. For now only show a message for the others. (Will use meta properties to serialize them later).

--- libs/core/Property.h
+++ libs/core/Property.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <core/core.h>
-#include <core/MetaProperties.h>
 #include <core/StringConversion.h>
 
 #include <memory>
--- libs/core/StringConversion.h
+++ libs/core/StringConversion.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "VectorWrapper.h"
+#include <core/MetaProperties.h>
 
 #include <sstream>
 #include <string>
@@ -12,6 +13,29 @@ namespace conversion
 
 	namespace details
 	{
+
+	template <class T>
+	class is_streamable
+	{
+		template<typename SS, typename TT> static auto testL(int) 
+			-> decltype( std::declval<SS&>() << std::declval<TT>(), std::true_type() );
+		template<typename, typename> static auto testL(...) -> std::false_type;
+
+		template<typename SS, typename TT> static auto testR(int) 
+			-> decltype( std::declval<SS&>() >> std::declval<TT&>(), std::true_type() );
+		template<typename, typename> static auto testR(...) -> std::false_type;
+
+	public:
+		static const bool value = decltype(testL<std::ostringstream,T>(0))::value && decltype(testR<std::istringstream,T>(0))::value;
+	};
+
+	template <class T>
+	class is_streamable<std::vector<T>> : public is_streamable<T> {};
+
+	template <class T>
+	class is_streamable<VectorWrapper<T>> : public is_streamable<T> {};
+
+	//****************************************************************************//
 
 	template <class T>
 	struct StringConversion
@@ -99,8 +123,34 @@ namespace conversion
 	template <class T>
 	struct StringConversion<VectorWrapper<T>>
 	{
-		static std::string toString(const VectorWrapper<T>& val) { return conversion::toString(val.value()); }
-		static void fromString(VectorWrapper<T>& val, const std::string& text) { conversion::fromString(val.value(), text); }
+		static std::string toString(const VectorWrapper<T>& val) { return StringConversion<T>::toString(val.value()); }
+		static void fromString(VectorWrapper<T>& val, const std::string& text) { StringConversion<T>::fromString(val.value(), text); }
+	};
+
+	//****************************************************************************//
+
+	template <class T, bool use_streams> struct ConversionSelector { };
+
+	template <class T>
+	struct ConversionSelector<T, true>
+	{
+		static std::string toString(const T& val) { return details::StringConversion<T>::toString(val); }
+		static void fromString(T& val, const std::string& text) { details::StringConversion<T>::fromString(val, text); }
+	};
+
+	template <class T>
+	struct ConversionSelector<T, false>
+	{
+		static std::string toString(const T& val) 
+		{
+			static_assert(false, "Cannot save this type");
+			return ""; 
+		}
+
+		static void fromString(T& val, const std::string& text) 
+		{
+			static_assert(false, "Cannot load this type");
+		}
 	};
 
 	}
@@ -110,12 +160,12 @@ namespace conversion
 	template <class T>
 	std::string toString(const T& val)
 	{
-		return details::StringConversion<T>::toString(val);
+		return details::ConversionSelector<T, details::is_streamable<T>::value>::toString(val);
 	}
 
 	template <class T>
 	void fromString(T& val, const std::string& text)
 	{
-		details::StringConversion<T>::fromString(val, text);
+		return details::ConversionSelector<T, details::is_streamable<T>::value>::fromString(val, text);
 	}
 }
